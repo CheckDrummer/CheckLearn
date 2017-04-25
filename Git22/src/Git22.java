2ffd84a352b35d054aeae6d80e2ba3acdfd05de6
user <user@21-000840>
2017-04-25 12:15:51 +0300

    Pr_5

--- Git22/src/Git22.java
+++ Git22/src/Git22.java
@@ -4,5 +4,11 @@ public class Git22 {
 		System.out.println("1");
 		
 		System.out.println("2");
+		
+		System.out.println("3");
+		
+		System.out.println("4");
+		
+		System.out.println("5");
 	}
 }
